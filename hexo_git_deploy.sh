7a708381a0d607d807a353a14cc43d69cd8a40b8
Leon Ma <leonme@126.com>
2018-12-17 00:25:41 +0800

    change configures

--- hexo_git_deploy.sh
+++ hexo_git_deploy.sh
@@ -4,4 +4,4 @@ hexo clean
 hexo generate 
 hexo deploy
 
-( cd ~/hexo_static ; git pull ; git push live master ; cp -r ~/hexo_static/ /var/www/hexo/)
+( cd ~/git/hexo_static ; git pull ; git push live master ; cp -r ~/git/hexo_static/ /var/www/hexo/)
